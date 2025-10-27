51c2ab31cf861f4e883d956de6fc09ecb58b7ccf
ROHIT SHARMA <RohitSharma.creator@gmail.com>
2025-10-27 10:30:00 +0000

    remove this (_for element h2_)

--- src/app/compulsory-deposit/page.tsx
+++ src/app/compulsory-deposit/page.tsx
@@ -22,7 +22,6 @@ export default function CompulsoryDepositPage() {
         
 
         <div className="my-16 md:my-24">
-            <h2 className="text-3xl font-bold font-headline text-center mb-12">Features & Benefits</h2>
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 <Card className="text-center">
                     <CardHeader>
