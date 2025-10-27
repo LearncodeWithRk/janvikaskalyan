1d27abdcb20590bedcc6ae0fa94d4f9ad4f33f03
ROHIT SHARMA <RohitSharma.creator@gmail.com>
2025-10-27 09:35:33 +0000

    remove this (_for element h2_)

--- src/app/fixed-deposit/page.tsx
+++ src/app/fixed-deposit/page.tsx
@@ -79,7 +79,6 @@ export default function FixedDepositPage() {
 
 
         <div className="my-16 md:my-24">
-            <h2 className="text-3xl font-bold font-headline text-center mb-12">Plan Details</h2>
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
                 <Card>
                     <CardHeader className="flex flex-row items-center gap-4">
