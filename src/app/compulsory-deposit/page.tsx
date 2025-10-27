c452e1067a918699be0b0ceee997a55568aa440a
ROHIT SHARMA <RohitSharma.creator@gmail.com>
2025-10-27 10:30:32 +0000

    add this text Compulsory Deposit : Return of Investment @ 9% per annum

--- src/app/compulsory-deposit/page.tsx
+++ src/app/compulsory-deposit/page.tsx
@@ -20,7 +20,7 @@ export default function CompulsoryDepositPage() {
 
       <div className="container mx-auto px-4 py-16 md:py-24">
         
-
+        <h2 className="text-3xl font-bold font-headline text-center mb-12">Compulsory Deposit : Return of Investment @ 9% per annum</h2>
         <div className="my-16 md:my-24">
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 <Card className="text-center">
