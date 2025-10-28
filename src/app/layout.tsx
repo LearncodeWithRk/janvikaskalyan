d4afe174bf268f2c9b90dae39c4306eada0c1c7b
ROHIT SHARMA <RohitSharma.creator@gmail.com>
2025-10-28 08:04:41 +0000

    remove kar do fevicon icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Header } from '@/components/layout/Header';
@@ -13,9 +14,6 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: 'Jan Vikas Kalyan Co-operative Society Ltd.',
   description: 'Jan Vikas Kalyan Co-operative Society Ltd. – a trusted one-stop financial partner in Delhi offering savings, deposits & member-friendly loans to over 30,000 members.',
-  icons: {
-    icon: 'https://res.cloudinary.com/learncodewithrk/image/upload/v1761631670/JVK%20IMG/logojvk_flqvna.png',
-  },
 };
 
 export default function RootLayout({
